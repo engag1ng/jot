6a87b929c27688d5284702de00f2a7afaf270d84
Constantin <131041909+engag1ng@users.noreply.github.com>
2026-03-05 20:53:13 +0100

    added command line and header

--- curse.c
+++ curse.c
@@ -1,16 +1,71 @@
 #include <ncurses.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main() {
+char **readFile(const char *filename, int *count) {
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("fopen");
+            return NULL;
+        }
+
+        char buffer[1024];
+        char **lines = NULL;
+        *count = 0;
+
+        while (fgets(buffer, sizeof(buffer), file)) {
+            char **temp = realloc(lines, (*count + 1) * sizeof(char *));
+            if (temp == NULL) {
+                for (int i = 0; i < *count; i++) free(lines[i]);
+                free(lines);
+                fclose(file);
+                return NULL;
+            }
+            lines = temp;
+            lines[*count] = strdup(buffer);
+            if (lines[*count] == NULL) {
+                for (int i = 0; i < *count; i++) free(lines[i]);
+                free(lines);
+                fclose(file);
+                return NULL;
+            }
+            (*count)++;
+        }
+
+        fclose(file);
+        return lines;
+    }
+
+    int writeFile(const char *filename, const char *data) {
+        FILE *file = fopen(filename, "w");
+        if (file == NULL) {
+            perror("fopen");
+            return 1;
+        }
+
+        fputs(data, file);
+        fclose(file);
+
+        return 0;
+    }
+
+int main(int argc, char* argv[]) {
+    char *filename = argv[1];
+    printf(filename);
+    int row, col;
+
     initscr();
+    getmaxyx(stdscr, row, col);
     raw();
     noecho();
-    printw("Todo\n");
+    printw("Jot - %s\n", filename);
+    refresh();
+    mvprintw(row-1, 0, "$ ");
     refresh();
 
     while (1) {
-        char cm[16];
-        char exit[16] = "exit";
+        char cm[128];
+        char exit[128] = "exit";
 
         echo();
         getstr(cm);
--- main.c
+++ main.c
@@ -1,18 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-    char todo[3][256];
-    int j;
-
-    for (j = 0; j < 3; j++) {
-        printf("What's your todo: \n");
-        fgets(todo[j], sizeof(todo[j]), stdin);
-        
-        int i;
-        for (i = 0; i < j+1; i++) {
-            printf("[ ] %s\n", todo[i]);    
+    char **readFile(const char *filename, int *count) {
+        FILE *file = fopen(filename, "r");
+        if (file == NULL) {
+            perror("fopen");
+            return NULL;
         }
+
+        char buffer[1024];
+        char **lines = NULL;
+        *count = 0;
+
+        while (fgets(buffer, sizeof(buffer), file)) {
+            char **temp = realloc(lines, (*count + 1) * sizeof(char *));
+            if (temp == NULL) {
+                for (int i = 0; i < *count; i++) free(lines[i]);
+                free(lines);
+                fclose(file);
+                return NULL;
+            }
+            lines = temp;
+            lines[*count] = strdup(buffer);
+            if (lines[*count] == NULL) {
+                for (int i = 0; i < *count; i++) free(lines[i]);
+                free(lines);
+                fclose(file);
+                return NULL;
+            }
+            (*count)++;
+        }
+
+        fclose(file);
+        return lines;
     }
+
+    int writeFile(const char *filename, const char *data) {
+        FILE *file = fopen(filename, "w");
+        if (file == NULL) {
+            perror("fopen");
+            return 1;
+        }
+
+        fputs(data, file);
+        fclose(file);
+
+        return 0;
+    }
+
+    int count;
+    char **lines = readFile("todo.txt", &count);
+    if (lines == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("%s", lines[i]);
+        free(lines[i]);
+    }
+    free(lines);
+    lines = NULL;
+
     return 0;
 }
