bf11921525231e60b12d9e612d15ff414a3b518e
Constantin <131041909+engag1ng@users.noreply.github.com>
2026-03-07 18:38:49 +0100

    can now display todo's form file

--- curse.c
+++ curse.c
@@ -36,34 +36,118 @@ char **readFile(const char *filename, int *count) {
         return lines;
     }
 
-    int writeFile(const char *filename, const char *data) {
-        FILE *file = fopen(filename, "w");
-        if (file == NULL) {
-            perror("fopen");
-            return 1;
-        }
-
-        fputs(data, file);
-        fclose(file);
-
-        return 0;
+int writeFile(const char *filename, const char *data) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        perror("fopen");
+        return 1;
     }
+
+    fputs(data, file);
+    fclose(file);
+
+    return 0;
+}
+
+char *multiply_character(char character, int multiplicator) {
+    if (multiplicator <= 0) {
+        return NULL;
+    }
+
+    char *string = malloc(multiplicator+1);
+    if (string == NULL) {
+        return NULL;
+    }
+
+    for (int j=0; j< multiplicator; j++) {
+        string[j] = character;
+    }
+    string[multiplicator] = '\0';
+    return string;
+}
 
 int main(int argc, char* argv[]) {
     char *filename = argv[1];
-    printf(filename);
     int row, col;
+    int count;
+    char **lines = readFile("test.txt", &count);
+    if (lines == NULL) {
+        return 1;
+    }
 
     initscr();
-    getmaxyx(stdscr, row, col);
+    keypad(stdscr, true);
     raw();
     noecho();
-    printw("Jot - %s\n", filename);
-    refresh();
-    mvprintw(row-1, 0, "$ ");
-    refresh();
+
+    int longest_name = 0;
+    int longest_row = 0;
+
+    for (int i = 0; i < count; i++) {
+        if (strlen(lines[i]) > longest_row) {
+            longest_row = strlen(lines[i]);
+        }
+        for (int j = 0; j < strlen(lines[i]); j++) {
+            char *ch = &lines[i][j];
+            if (*ch == ';') {
+                if (j > longest_name) {
+                    longest_name = j+1;
+                }
+            break;
+            }
+        }
+    }
+    
+    void draw_basic() {
+        getmaxyx(stdscr, row, col);
+        mvprintw(0, 0, "Jot - %s\n", filename);
+        char *topLine = malloc(longest_row+6);
+        if (!topLine) return;
+
+        topLine[0] = '|';
+
+        for (int i = 1; i < longest_row + 6; i++) {
+            topLine[i] = '-';
+        }
+
+        topLine[longest_row + 4] = '|';
+        topLine[longest_row + 5] = '\0';
+
+        mvprintw(3, 1, "%s", topLine);
+
+        move(2, 1);
+        printw("| Name");
+        printw(multiply_character(' ', longest_name-3));
+        printw("| Date added");
+        printw(multiply_character(' ', longest_row - longest_name-11));
+        addch('|');
+
+        for (int i = 0; i < count; i++) {
+            move(4+i, 0);
+            addch(i+'0');
+            printw("| ");
+            for(int j = 0; j < strlen(lines[i]); j++) {
+                char *ch = &lines[i][j];
+                if (*ch == ';') {
+                    if (j < longest_name) {
+                        printw(multiply_character(' ', longest_name-j));
+                    }
+                    printw(" | ");
+                } else {
+                    addch(lines[i][j]);
+                }                
+            }
+        }
+
+        mvprintw(row-1, 0, "$ ");
+
+        refresh();
+        free(topLine);
+        topLine = NULL;
+    }
 
     while (1) {
+        draw_basic();
         char cm[128];
         char exit[128] = "exit";
 
@@ -75,6 +159,7 @@ int main(int argc, char* argv[]) {
             break;
         }
 
+        erase();
     }
 
     return 0;
